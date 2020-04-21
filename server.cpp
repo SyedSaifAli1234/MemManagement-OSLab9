7c2dc26395813e7859f8c498e2be42fec5654327
SyedSaifAli1234 <52702293+SyedSaifAli1234@users.noreply.github.com>
2020-04-21 15:38:30 +0500

    .

--- server.cpp
+++ server.cpp
@@ -4,16 +4,16 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <iostream>
+#include <stdlib.h>
 using namespace std;
 
 
 int main(){
  
  char filename[13]="./myfile.txt";
- int fd=open(filename,  O_RD);
+ int fd=open(filename, O_RDONLY);
  
- if (fd==-1)
- {
+ if (fd==-1){
    cout<<"File could not open!"<<endl;
    return 1;
  }
@@ -21,21 +21,18 @@ int main(){
  char *map= (char*) mmap(NULL, 100, PROT_READ, MAP_SHARED, fd, 0);
  close(fd); //updated
  
- if (map==MAP_FAILED)
- {
+ if (map==MAP_FAILED){
    cout<<"File could not be memory mapped!"<<endl;
    return 1;
  }
 
- for (int i=0; i<10; i++)
- {
-   cout<<map[i]<<endl;
- }
-
- map[0]='B';
+for (int i=0; map[i]!='\0'; i++){
+	if(map[i] >= 48 && map[i] <= 57){
+		cout<<map[i]<<endl;
+	}
+}
  
  munmap(map, 10);
-
 
 }
 
