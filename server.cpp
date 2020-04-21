352578d4537085d9de78cd1073b4a1ced19913d0
SyedSaifAli1234 <52702293+SyedSaifAli1234@users.noreply.github.com>
2020-04-21 16:12:01 +0500

    Done

--- server.cpp
+++ server.cpp
@@ -9,30 +9,53 @@ using namespace std;
 
 
 int main(){
- 
- char filename[13]="./myfile.txt";
- int fd=open(filename, O_RDONLY);
- 
- if (fd==-1){
-   cout<<"File could not open!"<<endl;
-   return 1;
- }
- 
- char *map= (char*) mmap(NULL, 100, PROT_READ, MAP_SHARED, fd, 0);
- close(fd); //updated
- 
- if (map==MAP_FAILED){
-   cout<<"File could not be memory mapped!"<<endl;
-   return 1;
- }
 
-for (int i=0; map[i]!='\0'; i++){
-	if(map[i] >= 48 && map[i] <= 57){
-		cout<<map[i]<<endl;
+	char filename[13]="./myfile.txt";
+	char filename2[14]="./myfile2.txt";
+	int fd=open(filename, O_RDONLY);
+	int fd2=open(filename2, O_RDWR);
+
+	if (fd==-1){
+		cout<<"File could not open!"<<endl;
+		return 1;
 	}
-}
- 
- munmap(map, 10);
+	if (fd2==-1){
+		cout<<"File2 could not open!"<<endl;
+		return 1;
+	}
+
+	char *map= (char*) mmap(NULL, 100, PROT_READ, MAP_SHARED, fd, 0);
+	close(fd);
+	char *map2= (char*) mmap(NULL, 100, PROT_READ | PROT_WRITE, MAP_SHARED, fd2, 0);
+	close(fd2);
+
+	if (map==MAP_FAILED){
+		cout<<"File could not be memory mapped!"<<endl;
+		return 1;
+	}
+	if (map2==MAP_FAILED){
+		cout<<"File2 could not be memory mapped!"<<endl;
+		return 1;
+	}
+
+	for (int i=0; map[i]!='\0'; i++){
+		if(map[i] >= 48 && map[i] <= 57){
+			cout<<map[i]<<endl;
+		}
+	}
+
+	FILE *fp;
+	fp = fopen("./myfile2.txt", "w+");
+
+	for (int i=0; map[i]!='\0'; i++){
+		if(map[i] >= 48 && map[i] <= 57){
+			fputc(map[i], fp);
+		}
+	}
+	
+
+	munmap(map, 100);
+	munmap(map2, 100);
 
 }
 
